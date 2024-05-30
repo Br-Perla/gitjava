8f5c9df274795e2b62593a75ba8104bbbe64b490
Br-Perla <brianperla98@gmail.com>
2024-05-30 11:05:54 +0200

    ControllerFired

--- src/main/java/com/example/impiegati/Controller/impiegati.java
+++ src/main/java/com/example/impiegati/Controller/impiegati.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping (path = "impp")
+@RequestMapping (path = "imp")
 public class impiegati {
     @GetMapping (path = "/add")
     public @ResponseBody
-    String add(@RequestParam String name,@RequestParam String lavoro ) {
-        return " Il/la sig. "+name + " lavora come " +lavoro ;
+    String add(@RequestParam String name,@RequestParam String work ) {
+        return " Il/la sig. "+name + " lavora come " +work ;
+    }
+
+    @GetMapping ( path = "/fired")
+    public @ResponseBody
+    String remove(@RequestParam String name) {
+        return " Il/la sig. "+name + " è stato licenziato " ;
     }
     
 }
